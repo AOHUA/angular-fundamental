b39b3aba4c8d7c090633885ad747876c67d6b7ee
AOHUA <muaohua@gmail.com>
2017-03-08 09:35:55 +0800

    init commit

---
+++ app/eggly-app.start.js
@@ -0,0 +1,85 @@
+angular.module('Eggly', [
+
+])
+.controller('MainCtrl', function($scope) {
+  $scope.categories = [
+    {id: 0, name: 'development'},
+    {id: 1, name: 'design'},
+    {id: 2, name: 'Exercise'},
+    {id: 3, name: 'Humor'}
+  ];
+
+  $scope.bookmarks = [
+    {id: 0, title: 'Angular', category: 'development', url: 'Angular.com'},
+    {id: 1, title: 'AngularJS', category: 'design', url: 'react1.com'},
+    {id: 2, title: 'React', category: 'Exercise', url: 'react2.com'},
+    {id: 3, title: 'React2', category: 'Exercise', url: 'react2.com'},
+    {id: 4, title: 'Humor', category: 'Humor', url: 'react3.com'}
+  ];
+
+  $scope.currentCategory = null;
+
+  function setCurrentCategory(category) {
+    $scope.currentCategory = category;
+    resetState();
+  }
+
+  function isCurrentCategory(category) {
+    return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
+  }
+
+  $scope.setCurrentCategory = setCurrentCategory;
+  $scope.isCurrentCategory = isCurrentCategory;
+
+  $scope.isCreating = false;
+  $scope.isEditing = false;
+
+  function startCreating() {
+    $scope.isCreating = true;
+    $scope.isEditing = false;
+  }
+
+  function cancelCreating() {
+    $scope.isCreating = false;
+  }
+
+  function startEditing() {
+    $scope.isEditing = true;
+    $scope.isCreating = false;
+  }
+
+  function cancelEditing() {
+    $scope.isEditing = false;
+  }
+
+  function resetState() {
+    $scope.isEditing = false;
+    $scope.isCreating = false;
+  }
+
+  $scope.startCreating = startCreating;
+  $scope.cancelCreating = cancelCreating;
+  $scope.startEditing = startEditing;
+  $scope.cancelEditing = cancelEditing;
+
+  function resetForm(bookmark) {
+    bookmark.title = '';
+    bookmark.url = '';
+  }
+
+  function createNewBookmark(bookmark) {
+    var newBookmark = {
+      id: new Date(),
+      category: $scope.currentCategory.name,
+      url: bookmark.url,
+      title: bookmark.title
+    }
+    $scope.bookmarks.push(newBookmark);
+    resetForm(bookmark);
+  }
+
+
+
+  $scope.createNewBookmark = createNewBookmark;
+})
+;
